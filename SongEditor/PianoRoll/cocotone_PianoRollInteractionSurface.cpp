64b86c5b16072c7ab07785003c1775148d15291d
Tatsuya Shiozawa <tatsuya.shiozawa@cocotone.jp>
2024-07-17 13:36:54 +0900

    Chane add note event interaction.

--- SongEditor/PianoRoll/cocotone_PianoRollInteractionSurface.cpp
+++ SongEditor/PianoRoll/cocotone_PianoRollInteractionSurface.cpp
@@ -73,16 +73,19 @@ void PianoRollInteractionSurface::mouseUp(const juce::MouseEvent& mouseEvent)
         }
         else
         {
-            cctn::song::QueryForAddPianoRollNote query_for_add;
-            query_for_add.startTimeInSeconds = query.timeInSeconds;
-            query_for_add.endTimeInSeconds = query.timeInSeconds + 0.500;
-            query_for_add.noteNumber = query.noteNumber;
-            pianoRollEventDispatcher.emitAddNote(query_for_add);
+            if (mouseEvent.mods.isLeftButtonDown())
+            {
+                cctn::song::QueryForAddPianoRollNote query_for_add;
+                query_for_add.startTimeInSeconds = query.timeInSeconds;
+                query_for_add.endTimeInSeconds = query.timeInSeconds + 0.500;
+                query_for_add.noteNumber = query.noteNumber;
+                pianoRollEventDispatcher.emitAddNote(query_for_add);
 
-            cctn::song::QueryForFindPianoRollNote query_for_select;
-            query_for_select.noteNumber = query_for_add.noteNumber;
-            query_for_select.timeInSeconds = query_for_add.startTimeInSeconds;
-            pianoRollEventDispatcher.emitSelectNote(query_for_select);
+                cctn::song::QueryForFindPianoRollNote query_for_select;
+                query_for_select.noteNumber = query_for_add.noteNumber;
+                query_for_select.timeInSeconds = query_for_add.startTimeInSeconds;
+                pianoRollEventDispatcher.emitSelectNote(query_for_select);
+            }
         }
     }
 }
