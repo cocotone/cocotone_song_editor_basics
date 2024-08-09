effa63f606fcf19c5f6fae04b3d9cc6256aaf6e7
Tatsuya Shiozawa <tatsuya.shiozawa@cocotone.jp>
2024-08-10 01:05:12 +0900

    Remove unused.

--- SongEditor/Document/cocotone_SongDocumentTranspiler.h
+++ SongEditor/Document/cocotone_SongDocumentTranspiler.h
@@ -5,40 +5,25 @@ namespace cctn
 namespace song
 {
 
+//==============================================================================
+template <typename ArtefactDataType>
+class ITranspileTarget
+{
+public:
+    //==============================================================================
+    virtual ~ITranspileTarget() {}
+    virtual ArtefactDataType transpile(const cctn::song::SongDocument& sourceDocument) = 0;
+
+private:
+    //==============================================================================
+    JUCE_LEAK_DETECTOR(ITranspileTarget<ArtefactDataType>)
+};
+
+//==============================================================================
 class SongDocumentTranspiler
 {
 public:
     //==============================================================================
-    template <typename ArtefactDataType>
-    class ITranspileTarget
-    {
-    public:
-        //==============================================================================
-        virtual ~ITranspileTarget() {}
-        virtual ArtefactDataType transpile(const cctn::song::SongDocument& sourceDocument) = 0;
-
-    private:
-        //==============================================================================
-        JUCE_LEAK_DETECTOR(ITranspileTarget<ArtefactDataType>)
-    };
-
-    //==============================================================================
-    // Ad-hoc class placement.
-    class VoicevoxTranspileTarget
-        : public ITranspileTarget<juce::String>
-    {
-    public:
-        //==============================================================================
-        VoicevoxTranspileTarget() {}
-        virtual ~VoicevoxTranspileTarget() {}
-
-        //==============================================================================
-        virtual juce::String transpile(const cctn::song::SongDocument& sourceDocument) override;
-
-    private:
-        //==============================================================================
-        JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(VoicevoxTranspileTarget)
-    };
 
 private:
     //==============================================================================
